f81669899572b7692673b27e5dbd9035908adc00
ClaudirAzeredo <160553606+ClaudirAzeredo@users.noreply.github.com>
2024-11-24 23:54:58 -0300

    Initial commit

--- app/src/main/java/com/example/trabalhofaculfaz/CadastroAnimais.kt
+++ app/src/main/java/com/example/trabalhofaculfaz/CadastroAnimais.kt
@@ -9,16 +9,15 @@ import java.util.Calendar
 
 class CadastroAnimais : AppCompatActivity() {
 
-    // Declaração das variáveis para os elementos de UI
     private lateinit var spinnerSexo: Spinner
     private lateinit var etMachoDataVacina: EditText
     private lateinit var etMachoProximaVacina: EditText
     private lateinit var etMachoPeso: EditText
+    private lateinit var etTipoAnimal: EditText
     private lateinit var etMachoRegistro: EditText
     private lateinit var btnRegister: Button
     private lateinit var tvCadastroInfo: TextView
 
-    // Lista para armazenar os animais cadastrados
     private val animaisCadastrados = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,29 +26,23 @@ class CadastroAnimais : AppCompatActivity() {
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true) // Adiciona a seta de voltar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-
-        // Ação ao clicar na seta de voltar
         toolbar.setNavigationOnClickListener {
-            finish() // Fecha a activity e volta para a anterior
+            finish()
         }
 
-        // Inicializando os elementos do layout
         spinnerSexo = findViewById(R.id.spinnerSexo)
         etMachoDataVacina = findViewById(R.id.etMachoDataVacina)
         etMachoProximaVacina = findViewById(R.id.etMachoProximaVacina)
         etMachoPeso = findViewById(R.id.etMachoPeso)
         etMachoRegistro = findViewById(R.id.etMachoRegistro)
+        etTipoAnimal = findViewById(R.id.etTipoAnimal)
         btnRegister = findViewById(R.id.btnRegister)
         tvCadastroInfo = findViewById(R.id.tvCadastroInfo)
 
-        // Configurando os campos de data com DatePicker
-        etMachoDataVacina.setOnClickListener {
-            showDatePickerDialog { date ->
-                etMachoDataVacina.setText(date)
-            }
-        }
+        val selectedAnimais = intent.getStringExtra("selectedAnimais")
+        etTipoAnimal.setText(selectedAnimais)
 
         etMachoProximaVacina.setOnClickListener {
             showDatePickerDialog { date ->
@@ -57,54 +50,49 @@ class CadastroAnimais : AppCompatActivity() {
             }
         }
 
-        // Configuração do Spinner
         val sexoOptions = listOf("Selecione o sexo", "Macho", "Fêmea")
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, sexoOptions)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerSexo.adapter = adapter
 
-        // Configuração do botão de cadastro
         btnRegister.setOnClickListener {
-            // Obtendo os valores inseridos pelo usuário
+
+            val tipoAnimais = etTipoAnimal.text.toString()
             val sexoSelecionado = spinnerSexo.selectedItem.toString()
             val dataVacina = etMachoDataVacina.text.toString()
             val proximaVacina = etMachoProximaVacina.text.toString()
             val peso = etMachoPeso.text.toString()
             val registro = etMachoRegistro.text.toString()
 
-            // Verificando se todos os campos estão preenchidos
-            if (sexoSelecionado == "Selecione o sexo" || dataVacina.isEmpty() || proximaVacina.isEmpty() || peso.isEmpty() || registro.isEmpty()) {
-                Toast.makeText(this, "Preencha todos os campos corretamente", Toast.LENGTH_SHORT).show()
-            } else {
-                // Criando uma string para o animal
-                val cadastroInfo = """
-                    Sexo: $sexoSelecionado
-                    Data da Vacina: $dataVacina
-                    Próxima Vacina: $proximaVacina
-                    Peso: $peso kg
-                    N° Registro: $registro
-                """.trimIndent()
+            if (sexoSelecionado == "Selecione o sexo" || tipoAnimais.isNotEmpty() ||dataVacina.isNotEmpty() || proximaVacina.isNotEmpty()
+                || peso.isNotEmpty() || registro.isNotEmpty())
 
-                // Adicionando o cadastro à lista de animais
-                animaisCadastrados.add(cadastroInfo)
+            else {
+                val animalInfo = """
+            Tipo de Animal: $tipoAnimais
+            Sexo: $sexoSelecionado
+            Data da Vacina: $dataVacina
+            Próxima Vacina: $proximaVacina
+            Peso: $peso kg
+            N° Registro: $registro
+        """.trimIndent()
 
-                // Atualizando o TextView com a lista de animais cadastrados
+                animaisCadastrados.add(animalInfo)
+
                 tvCadastroInfo.text = animaisCadastrados.joinToString(separator = "\n\n") { it }
 
-                // Limpando os campos após o cadastro
-                spinnerSexo.setSelection(0) // Reseta o Spinner
+                spinnerSexo.setSelection(0)
                 etMachoDataVacina.text.clear()
                 etMachoProximaVacina.text.clear()
                 etMachoPeso.text.clear()
                 etMachoRegistro.text.clear()
 
-                // Exibindo um Toast de confirmação
+
                 Toast.makeText(this, "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    // Método para exibir o DatePickerDialog
     private fun showDatePickerDialog(onDateSet: (String) -> Unit) {
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
@@ -114,10 +102,10 @@ class CadastroAnimais : AppCompatActivity() {
         val datePickerDialog = DatePickerDialog(
             this,
             { _, selectedYear, selectedMonth, selectedDay ->
-                // Formatar a data para o formato desejado
+
                 val formattedDate = "${selectedDay.toString().padStart(2, '0')}/" +
                         "${(selectedMonth + 1).toString().padStart(2, '0')}/$selectedYear"
-                onDateSet(formattedDate) // Passa a data formatada para o EditText
+                onDateSet(formattedDate)
             },
             year, month, day
         )
--- app/src/main/java/com/example/trabalhofaculfaz/CadastroColheitas.kt
+++ app/src/main/java/com/example/trabalhofaculfaz/CadastroColheitas.kt
@@ -22,17 +22,14 @@ class CadastroColheitas : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cadastro_planta)
 
-        // Configurando a Toolbar
         val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        // Lidando com a ação de voltar
         toolbar.setNavigationOnClickListener {
             finish()
         }
 
-        // Inicializando os componentes da interface
         imagePlantacao = findViewById(R.id.imagePlantacao)
         etTipoColheita = findViewById(R.id.etTipoColheita)
         etDataPlantio = findViewById(R.id.etDataPlantio)
@@ -42,7 +39,6 @@ class CadastroColheitas : AppCompatActivity() {
         btnAddPlantInfo = findViewById(R.id.btnAddPlantInfo)
         listViewPlantInfo = findViewById(R.id.listViewPlantInfo)
 
-        // Configurando os campos de data com DatePicker
         etDataPlantio.setOnClickListener {
             showDatePickerDialog { date ->
                 etDataPlantio.setText(date)
@@ -55,18 +51,14 @@ class CadastroColheitas : AppCompatActivity() {
             }
         }
 
-        // Recuperando o tipo de colheita passado pela Intent
         val selectedColheita = intent.getStringExtra("selectedColheita")
         etTipoColheita.setText(selectedColheita)
 
-        // Inicializando a lista de informações
         plantInfoList = ArrayList()
 
-        // Configurando o adapter da ListView
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, plantInfoList)
         listViewPlantInfo.adapter = adapter
 
-        // Configurando o botão para adicionar informações
         btnAddPlantInfo.setOnClickListener {
             val tipoColheita = etTipoColheita.text.toString()
             val dataPlantio = etDataPlantio.text.toString()
@@ -74,42 +66,34 @@ class CadastroColheitas : AppCompatActivity() {
             val dataProximoInsumo = etDataProximoInsumo.text.toString()
             val quantidadeAreaColhida = etQuantidadeAreaColhida.text.toString()
 
-            // Validando se os campos não estão vazios
             if (tipoColheita.isNotEmpty() && dataPlantio.isNotEmpty() && tipoInsumos.isNotEmpty() &&
                 dataProximoInsumo.isNotEmpty() && quantidadeAreaColhida.isNotEmpty()
             ) {
-                // Criando a string com as informações inseridas
                 val plantInfo = "Tipo de Colheita: $tipoColheita\n" +
                         "Data de Plantio: $dataPlantio\n" +
                         "Tipo de Insumos: $tipoInsumos\n" +
                         "Data do Próximo Insumo: $dataProximoInsumo\n" +
                         "Quantidade de Área Colhida: $quantidadeAreaColhida"
 
-                // Adicionando a informação à lista
                 plantInfoList.add(plantInfo)
 
-                // Notificando o adapter para atualizar a ListView
                 adapter.notifyDataSetChanged()
 
-                // Limpando os campos de entrada
                 etTipoColheita.text.clear()
                 etDataPlantio.text.clear()
                 etTipoInsumos.text.clear()
                 etDataProximoInsumo.text.clear()
                 etQuantidadeAreaColhida.text.clear()
 
-                // Exibindo mensagem de sucesso
                 Toast.makeText(this, "Informações cadastradas com sucesso!", Toast.LENGTH_SHORT)
                     .show()
             } else {
-                // Caso algum campo esteja vazio, mostrar um alerta
                 Toast.makeText(this, "Por favor, preencha todos os campos!", Toast.LENGTH_SHORT)
                     .show()
             }
         }
     }
 
-    // Método para exibir o DatePickerDialog
     private fun showDatePickerDialog(onDateSet: (String) -> Unit) {
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
@@ -119,10 +103,9 @@ class CadastroColheitas : AppCompatActivity() {
         val datePickerDialog = DatePickerDialog(
             this,
             { _, selectedYear, selectedMonth, selectedDay ->
-                // Formatar a data para o formato desejado
                 val formattedDate = "${selectedDay.toString().padStart(2, '0')}/" +
                         "${(selectedMonth + 1).toString().padStart(2, '0')}/$selectedYear"
-                onDateSet(formattedDate) // Passa a data formatada para o EditText
+                onDateSet(formattedDate)
             },
             year, month, day
         )
