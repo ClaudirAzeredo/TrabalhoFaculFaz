23d5c5a0b78efac54c6e79907c5bf643b47d6d68
Claudir Junior Azeredo <160553606+ClaudirAzeredo@users.noreply.github.com>
2024-11-25 12:54:34 -0300

    MyFirebaseMessagingService.kt

--- app/src/main/java/com/example/trabalhofaculfaz/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/trabalhofaculfaz/MyFirebaseMessagingService.kt
@@ -6,10 +6,8 @@ import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
         override fun onMessageReceived(remoteMessage: RemoteMessage) {
-            // Lógica para lidar com notificações push recebidas
         }
 
         override fun onNewToken(token: String) {
-            // Salve o novo token para notificações push
         }
     }
